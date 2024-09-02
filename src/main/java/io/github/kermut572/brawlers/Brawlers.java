17f6e9ee9da642390c64567895ec48a0615bbfb3
Alexandre Navez <119766063+Kermut572@users.noreply.github.com>
2024-09-02 18:44:54 +0200

    init

---
+++ src/main/java/io/github/kermut572/brawlers/Brawlers.java
@@ -0,0 +1,17 @@
+package io.github.kermut572.brawlers;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Brawlers extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
